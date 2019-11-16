e78329cfed070ece77900e540f9bcda9d83b00fa
Georgii Sirotkin <georgiysirotkin@gmail.com>
2019-11-16 11:16:25 -0500

    Create dashboard with two pages

---
+++ src/AddPoll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AddPoll extends React.Component {
+    render() {
+        return (
+            <h1 className="h2">New Poll</h1>
+        )
+    }
+}
+
+export default AddPoll;

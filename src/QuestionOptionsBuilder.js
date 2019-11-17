b56d23fcbfa398f27f6682f5adf5d872c6037608
Georgii Sirotkin <georgiysirotkin@gmail.com>
2019-11-17 00:05:44 -0500

    Enable user to add "Other" option

--- src/QuestionOptionsBuilder.js
+++ src/QuestionOptionsBuilder.js
@@ -3,11 +3,13 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import nanoid from 'nanoid';
+import orderBy from 'lodash.orderby';
 
-export default class MultipleChoiceQuestionBuilder extends React.Component {
+export default class QuestionOptionsBuilder extends React.Component {
   constructor(props) {
     super(props);
     this.addOption = this.addOption.bind(this);
+    this.addOtherOption = this.addOtherOption.bind(this);
   }
 
   handleOptionTextChange(id, text) {
@@ -30,6 +32,19 @@ export default class MultipleChoiceQuestionBuilder extends React.Component {
     this.props.onDataChange(data);
   }
 
+  addOtherOption() {
+    const options = this.props.data.options.concat([
+      {
+        id: nanoid(),
+        text: 'Other',
+        is_other: true
+      }
+    ]);
+
+    const data = {...this.props.data, options};
+    this.props.onDataChange(data);
+  }
+
   removeOption(id) {
     const options = this.props.data.options.filter(option => option.id !== id);
     const data = {...this.props.data, options};
@@ -37,14 +52,16 @@ export default class MultipleChoiceQuestionBuilder extends React.Component {
   }
 
   render() {
-    const options = this.props.data.options.map((option) => {
+    const sortedOptions = orderBy(this.props.data.options, ['is_other'], ['desc']);
+    const options = sortedOptions.map((option) => {
       return (
         <Form.Group key={option.id}>
           <InputGroup>
             <Form.Control
               type="text"
               value={option.text}
-              onChange={(e) => this.handleOptionTextChange(option.id, e.target.value)} />
+              onChange={(e) => this.handleOptionTextChange(option.id, e.target.value)}
+              disabled={option.is_other}/>
             <InputGroup.Append>
               <Button variant="outline-secondary" onClick={() => this.removeOption(option.id)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
@@ -55,11 +72,14 @@ export default class MultipleChoiceQuestionBuilder extends React.Component {
       )
     });
 
+    const hasOtherOption = this.props.data.options.some(option => option.is_other);
+
     return (
       <div>
         {options}
         <Form.Group>
           <Button variant="outline-primary" onClick={this.addOption}>Add option</Button>
+          {!hasOtherOption && <Button variant="outline-primary" className="ml-2" onClick={this.addOtherOption}>Add "Other"</Button>}
         </Form.Group>
       </div>
     );

4b48a0b80971537636402b26b3f4086943403b63
Georgii Sirotkin <georgiysirotkin@gmail.com>
2019-11-21 13:39:49 -0500

    Update production api url

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,3 @@
 export default {
-  apiUrl: process.env.NODE_ENV === 'production' ? 'https://poll-demo-1-api.herokuapp.com' : 'http://poll-api.test/api',
+  apiUrl: process.env.NODE_ENV === 'production' ? 'https://poll-demo-1-api.herokuapp.com/api' : 'http://poll-api.test/api',
 }

dbe8e374e61ad156dfaaa8e69a00079754e753af
Georgii Sirotkin <georgiysirotkin@gmail.com>
2020-01-24 16:37:36 -0500

    Convert Dashboard component to function component

--- src/admin/Dashboard.js
+++ src/admin/Dashboard.js
@@ -10,82 +10,74 @@ import {
   Link,
   NavLink,
   Redirect,
-  withRouter
+  useRouteMatch
 } from "react-router-dom";
 
-class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Dashboard({isLoggedIn, onLogOut}) {
+  const match = useRouteMatch();
 
-    this.logOut = this.logOut.bind(this);
-  }
-
-  logOut() {
+  function logOut() {
     const url = Config.apiUrl + '/auth/logout';
-    axios.post(url, {}, {withCredentials: true});
+    axios.post(url, {}, { withCredentials: true });
 
-    this.props.onLogOut();
+    onLogOut();
   }
 
-  render() {
-    if (!this.props.isLoggedIn) {
-      return (
-        <Redirect
-            to='/admin/login'
-        />
-      )
-    }
-
+  if (!isLoggedIn) {
     return (
-      <>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <Link to={`${this.props.match.url}`} className="navbar-brand col-sm-3 col-md-2 mr-0">Dashboard</Link>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap">
-              <button className="nav-link btn btn-link" onClick={this.logOut}>Sign out</button>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="container-fluid">
-          <div className="row">
-            <nav className="col-md-2 d-none d-md-block bg-light sidebar">
-              <div className="sidebar-sticky">
-                <ul className="nav flex-column">
-                  <li className="nav-item">
-                    <NavLink to={`${this.props.match.url}/polls`} className="nav-link">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check-square"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
-                      Polls
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink to={`${this.props.match.url}/create-poll`} className="nav-link">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file-plus"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="12" y1="18" x2="12" y2="12"></line><line x1="9" y1="15" x2="15" y2="15"></line></svg>
-                      New Poll
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-
-            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-              <Switch>
-                <Route path={`${this.props.match.path}/create-poll`}>
-                  <AddPoll />
-                </Route>
-                <Route path={`${this.props.match.path}/polls/:id`}>
-                  <Poll />
-                </Route>
-                <Route path={`${this.props.match.path}/polls`}>
-                  <Polls />
-                </Route>
-              </Switch>
-            </main>
-          </div>
-        </div>
-      </>
-    );
+      <Redirect
+          to='/admin/login'
+      />
+    )
   }
-}
 
-export default withRouter(Dashboard);
+  return (
+    <>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <Link to={match.url} className="navbar-brand col-sm-3 col-md-2 mr-0">Dashboard</Link>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <button className="nav-link btn btn-link" onClick={logOut}>Sign out</button>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container-fluid">
+        <div className="row">
+          <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+            <div className="sidebar-sticky">
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <NavLink to={`${match.url}/polls`} className="nav-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check-square"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
+                    Polls
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to={`${match.url}/create-poll`} className="nav-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file-plus"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="12" y1="18" x2="12" y2="12"></line><line x1="9" y1="15" x2="15" y2="15"></line></svg>
+                    New Poll
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+            <Switch>
+              <Route path={`${match.path}/create-poll`}>
+                <AddPoll />
+              </Route>
+              <Route path={`${match.path}/polls/:id`}>
+                <Poll />
+              </Route>
+              <Route path={`${match.path}/polls`}>
+                <Polls />
+              </Route>
+            </Switch>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+};
